9e83f46275ddef8d9357d9f1185cefbc252015cf
Rakions <ogmohammed2004@hotmail.com>
2023-12-07 19:09:22 +0100

    funcion para hacer compras

--- server/Services/ObraService.js
+++ server/Services/ObraService.js
@@ -84,24 +84,21 @@ const obraService = {
     });
     saveObrasToJson(obras);
   },
-  // makeCompra: function (id_obra, id_asiento) {
-  //   let obras = this.getAllObras();
-  //   console.log(obras);
-  //   obras.map((obra) => {
-  //     if (obra.id === id_obra) {
-  //       obra.asientos.map((asiento) => {
-  //         if (asiento.numero === id_asiento) {
-  //           asiento.ocupado = true;
-  //         }
-  //       });
-  //     }
-  //   });
-  //   saveObrasToJson(obra);
-  // },
+  makeCompra: function (id_obra, id_asiento) {
+    let obras = this.getAllObras();
+    obras.map((obra) => {
+      if (obra.id === id_obra) {
+        obra.asientos.map((asiento) => {
+          if (asiento.numero === id_asiento) {
+            asiento.ocupado = true;
+          }
+        });
+      }
+    });
+    saveObrasToJson(obras);
+  },
 };
 
 var listaObras = getObrasFromJSON();
-
-// obraService.makeCompra("ab08d8fe-d2d3-4e44-ab29-4c95047a09b2", 0);
 
 module.exports = obraService;
