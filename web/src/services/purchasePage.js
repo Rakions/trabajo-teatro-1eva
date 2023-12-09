d8265b135aff6799dc8312640168b238f25e75e5
Omar <118843137+Rakions@users.noreply.github.com>
2023-12-09 21:55:37 +0100

    mejorado un poco el codigo

--- web/src/services/purchasePage.js
+++ web/src/services/purchasePage.js
@@ -1,18 +1,18 @@
-const asientosSeleccionados = localStorage.getItem("asientos");
-const id = localStorage.getItem("id");
+const selectedSeats = localStorage.getItem("asientos");
+const obraId = localStorage.getItem("id");
 
-async function makeCompra(id_asiento) {
-  var myHeaders = new Headers();
+async function makePurchase(seatId) {
+  const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
-  id_asiento = parseInt(id_asiento, 10);
+  const id_asiento = parseInt(seatId, 10);
 
-  var raw = JSON.stringify({
-    id_obra: id,
+  const raw = JSON.stringify({
+    id_obra: obraId,
     id_asiento: id_asiento,
   });
 
-  var requestOptions = {
+  const requestOptions = {
     method: "PUT",
     headers: myHeaders,
     body: raw,
@@ -20,27 +20,31 @@ async function makeCompra(id_asiento) {
 
   try {
     const response = await fetch("http://localhost:3000/obras/compra", requestOptions);
+    if (!response.ok) {
+      throw new Error(`Failed to make a purchase for seat ${id_asiento}.`);
+    }
     const result = await response.text();
     return JSON.parse(result);
   } catch (error) {
-    console.error("Error en makeCompra para el asiento", id_asiento, ":", error.message);
+    console.error(`Error in makePurchase for seat ${id_asiento}: ${error.message}`);
     throw error;
   }
 }
 
-async function comprar() {
-  var numerosArray = asientosSeleccionados.split(",");
-  const promesasCompras = numerosArray.map((numero) => makeCompra(numero));
+async function buyTickets() {
+  const seatNumbers = selectedSeats.split(",");
+  const purchasePromises = seatNumbers.map((number) => makePurchase(number));
 
   try {
-    const resultados = await Promise.all(promesasCompras);
+    const results = await Promise.all(purchasePromises);
+    // Handle the results if needed
   } catch (error) {
-    console.error("Error al realizar las compras:", error);
+    console.error("Error while making purchases:", error);
   }
 }
 
 document.querySelector(".purchase_buttons_purchase").addEventListener("click", () => {
-  const email = document.getElementById("email").value;
-  localStorage.setItem("email", email);
-  comprar();
+  const userEmail = document.getElementById("email").value;
+  localStorage.setItem("email", userEmail);
+  buyTickets();
 });
