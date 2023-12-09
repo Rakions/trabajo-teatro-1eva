e3d92e170e482408a998047727a14f44f6029b72
Omar <118843137+Rakions@users.noreply.github.com>
2023-12-09 21:50:27 +0100

    ya se muestra el correo del usuario que ha comprado

--- web/src/services/purchasePage.js
+++ web/src/services/purchasePage.js
@@ -1,42 +1,6 @@
 const asientosSeleccionados = localStorage.getItem("asientos");
 const id = localStorage.getItem("id");
 
-async function createUser() {
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-
-  var numerosArray = asientosSeleccionados.split(",");
-  const idPelicula = id;
-
-  const entradas = numerosArray.map((numero) => {
-    return {
-      numero: parseInt(numero, 10),
-      pelicula: idPelicula,
-    };
-  });
-
-  var raw = JSON.stringify({
-    nombre: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    contra: 123,
-    entradas: entradas,
-  });
-
-  console.log(raw);
-
-  var requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: raw,
-    redirect: "manual",
-  };
-
-  fetch("http://localhost:3000/users", requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.log("error", error));
-}
-
 async function makeCompra(id_asiento) {
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
@@ -52,18 +16,10 @@ async function makeCompra(id_asiento) {
     method: "PUT",
     headers: myHeaders,
     body: raw,
-    redirect: "manual",
   };
 
   try {
     const response = await fetch("http://localhost:3000/obras/compra", requestOptions);
-
-    if (!response.ok) {
-      throw new Error(
-        `Error al realizar la compra para el asiento ${id_asiento}. Código de estado: ${response.status}`
-      );
-    }
-
     const result = await response.text();
     return JSON.parse(result);
   } catch (error) {
@@ -78,12 +34,13 @@ async function comprar() {
 
   try {
     const resultados = await Promise.all(promesasCompras);
-    createUser();
   } catch (error) {
     console.error("Error al realizar las compras:", error);
   }
 }
 
 document.querySelector(".purchase_buttons_purchase").addEventListener("click", () => {
+  const email = document.getElementById("email").value;
+  localStorage.setItem("email", email);
   comprar();
 });
