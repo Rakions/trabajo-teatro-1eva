3c1068de1f1bfc1c522a183569107e42daa2489c
Omar <118843137+Rakions@users.noreply.github.com>
2023-12-09 16:54:55 +0100

    arreglado error en el controller

--- server/Controller/AsientosController.js
+++ server/Controller/AsientosController.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const asientoService = require("../Services/AsientoService");
 
-router.get("/", (req, res) => {
-  res.send(asientoService.getAllAsientos());
+router.get("/:id", (req, res) => {
+  res.send(asientoService.getAllAsientos(req.params.id));
 });
 
-router.get("/:id", (req, res) => {
-  res.send(asientoService.getAsientoById(req.params.id));
+router.get("/", (req, res) => {
+  const { id_obra, id_asiento } = req.body;
+  res.send(asientoService.getAsientoById(id_obra, id_asiento));
 });
 
 module.exports = router;
