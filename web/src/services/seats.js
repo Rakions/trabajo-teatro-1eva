29a0db1460b1a81231c6e43ed2c05919c6487261
Zas <102923191+meeeww@users.noreply.github.com>
2023-12-09 23:49:51 +0100

    Ultimos fixes para la entrega

--- web/src/services/seats.js
+++ web/src/services/seats.js
@@ -1,48 +1,49 @@
 let id = "";
+let totalPrecioAsientos = 0;
 
 async function getObraById() {
-  id = localStorage.getItem("id");
-  var requestOptions = {
-    method: "GET",
-    redirect: "follow",
-  };
+    id = localStorage.getItem("id");
+    var requestOptions = {
+        method: "GET",
+        redirect: "follow",
+    };
 
-  const url = "http://localhost:3000/obras/" + id;
-  const response = await fetch(url, requestOptions);
-  const result = await response.json();
-  return result;
+    const url = "http://localhost:3000/obras/" + id;
+    const response = await fetch(url, requestOptions);
+    const result = await response.json();
+    return result;
 }
 
 async function getAsientoPorId(ID_asiento) {
-  id = localStorage.getItem("id");
-  const bodyData = {
-    id_obra: id,
-    id_asiento: ID_asiento,
-  };
-  var requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(bodyData),
-    redirect: "follow",
-  };
+    id = localStorage.getItem("id");
+    const bodyData = {
+        id_obra: id,
+        id_asiento: ID_asiento,
+    };
+    var requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bodyData),
+        redirect: "follow",
+    };
 
-  const url = "http://localhost:3000/asientos";
-  const response = await fetch(url, requestOptions);
-  const result = await response.json();
-  return result;
+    const url = "http://localhost:3000/asientos";
+    const response = await fetch(url, requestOptions);
+    const result = await response.json();
+    return result;
 }
 
 var asientosSeleccionados = [];
 
 async function cargarAsientos() {
-  const obra = await getObraById();
-  const asientos = obra[0].asientos;
+    const obra = await getObraById();
+    const asientos = obra[0].asientos;
 
-  const functionInfo = document.querySelector(".seats_functionInfo");
-  functionInfo.innerHTML = "";
-  functionInfo.innerHTML += `
+    const functionInfo = document.querySelector(".seats_functionInfo");
+    functionInfo.innerHTML = "";
+    functionInfo.innerHTML += `
     <div class="function">
       <img src="${obra[0].image}" alt="" />
       <div>
@@ -52,76 +53,79 @@ async function cargarAsientos() {
       <input type="hidden" id="id_obra" value="${obra[0].id}"/>
     </div>`;
 
-  const asientosHTML = document.querySelector(".seats_all_seats_image");
+    const asientosHTML = document.querySelector(".seats_all_seats_image");
 
-  asientos.map((asiento) => {
-    asientosHTML.innerHTML += `<button class="seat" id="${asiento.numero}" value='${asiento.precio}'></button>`;
-  });
+    asientos.map((asiento) => {
+        asientosHTML.innerHTML += `<button class="seat" id="${asiento.numero}" value='${asiento.precio}'></button>`;
+    });
 
-  let totalPrecioAsientos = 0;
-  const precioTotal = document.querySelector(".selected_seats_total > h2");
+    const precioTotal = document.querySelector(".selected_seats_total > h2");
 
-  const botones = asientosHTML.querySelectorAll(".seat");
-  botones.forEach((boton) => {
-    const numeroAsiento = parseInt(boton.id, 10);
-    const asientoCorrespondiente = asientos.find((asiento) => asiento.numero === numeroAsiento);
-
-    if (asientoCorrespondiente.ocupado) {
-      boton.style.backgroundColor = "red";
-      boton.disabled = true;
-    } else {
-      boton.addEventListener("click", () => {
+    const botones = asientosHTML.querySelectorAll(".seat");
+    botones.forEach((boton) => {
         const numeroAsiento = parseInt(boton.id, 10);
+        const asientoCorrespondiente = asientos.find((asiento) => asiento.numero === numeroAsiento);
 
-        if (!asientosSeleccionados.includes(numeroAsiento)) {
-          asientosSeleccionados.push(numeroAsiento);
-          boton.style.backgroundColor = "#62ae00";
-          totalPrecioAsientos += asientoCorrespondiente.precio;
+        if (asientoCorrespondiente.ocupado) {
+            boton.style.backgroundColor = "red";
+            boton.disabled = true;
         } else {
-          asientosSeleccionados.splice(asientosSeleccionados.indexOf(numeroAsiento), 1);
-          boton.style.backgroundColor = "var(--color-principal)";
-          totalPrecioAsientos -= asientoCorrespondiente.precio;
+            boton.addEventListener("click", () => {
+                const numeroAsiento = parseInt(boton.id, 10);
+
+                if (!asientosSeleccionados.includes(numeroAsiento)) {
+                    asientosSeleccionados.push(numeroAsiento);
+                    boton.style.backgroundColor = "#62ae00";
+                    totalPrecioAsientos += asientoCorrespondiente.precio;
+                } else {
+                    asientosSeleccionados.splice(asientosSeleccionados.indexOf(numeroAsiento), 1);
+                    boton.style.backgroundColor = "var(--color-principal)";
+                    totalPrecioAsientos -= asientoCorrespondiente.precio;
+                }
+                actualizarLista();
+                precioTotal.innerHTML = "Total: " + totalPrecioAsientos + "€";
+            });
         }
-        actualizarLista();
-        precioTotal.innerHTML = "Total: " + totalPrecioAsientos + "€";
-      });
-    }
-  });
+    });
 }
 
 function actualizarLista() {
-  const listaAsientos = document.querySelector(".selected_seats_list");
-  listaAsientos.innerHTML = "";
+    const listaAsientos = document.querySelector(".selected_seats_list");
+    listaAsientos.innerHTML = "";
 
-  asientosSeleccionados.forEach(async (asiento) => {
-    const infoAsiento = await getAsientoPorId(asiento);
+    asientosSeleccionados.forEach(async (asiento) => {
+        const infoAsiento = await getAsientoPorId(asiento);
 
-    listaAsientos.innerHTML += `
+        listaAsientos.innerHTML += `
       <li class="selected_seats_item">
         <div>Asiento: ${infoAsiento.numero}</div>
         <div>
           <p>${infoAsiento.precio + "€"}</p>
-          <button onclick="eliminarAsiento(${infoAsiento.numero})">X</button>
+          <button onclick="eliminarAsiento(${infoAsiento.numero}, ${infoAsiento.precio})">X</button>
         </div>
       </li>
     `;
-  });
+    });
 }
 
-function eliminarAsiento(id_asiento) {
-  asientosSeleccionados = asientosSeleccionados.filter((asiento) => asiento !== id_asiento);
+function eliminarAsiento(id_asiento, precio_asiento) {
+    const precioTotal = document.querySelector(".selected_seats_total > h2");
 
-  const boton = document.getElementById(id_asiento);
-  if (boton) {
-    boton.style.backgroundColor = "var(--color-principal)";
-  }
+    asientosSeleccionados = asientosSeleccionados.filter((asiento) => asiento !== id_asiento);
 
-  actualizarLista();
+    const boton = document.getElementById(id_asiento);
+    if (boton) {
+        boton.style.backgroundColor = "var(--color-principal)";
+        totalPrecioAsientos = totalPrecioAsientos - precio_asiento;
+        precioTotal.innerHTML = "Total: " + totalPrecioAsientos + "€";
+    }
+
+    actualizarLista();
 }
 
 const botonPagar = document.querySelector(".selected_seats_total > button");
 botonPagar.addEventListener("click", () => {
-  localStorage.setItem("asientos", asientosSeleccionados);
+    localStorage.setItem("asientos", asientosSeleccionados);
 });
 
 cargarAsientos();
